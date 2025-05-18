4cc9caf7daf1adca063864d3615cf23b4d66d9f7
Tyrese-Mv <musawenkosi106@gmail.com>
2025-05-18 18:07:47 +0200

    added local db which provides a runtime database as a hashmap

---
+++ src/model/LocalDB.ts
@@ -0,0 +1,32 @@
+import { DB } from "./util";
+import { Task } from "./TaskClass";
+
+export class LocalDB{
+    static #instance: LocalDB;
+    private database: DB = {};
+
+    private constructor() {
+    }
+
+    public static get instance(): LocalDB {
+        if (!LocalDB.#instance) {
+            LocalDB.#instance = new LocalDB();
+        }
+
+        return LocalDB.#instance;
+    }
+
+    public AddTask(userTask : Task): void{
+        const taskKey = userTask.GetDateAsKey();
+
+        if (!this.database[taskKey]) {
+            this.database[taskKey] = [];
+        }
+    
+        this.database[taskKey].push(userTask);
+    }
+
+    public toString(): string {
+        return JSON.stringify(this.database, null, 2);
+      }
+}
