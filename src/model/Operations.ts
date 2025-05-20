ea823062ea224f9c2cef34bb4e3c08ddd34b3452
Tyrese-Mv <musawenkosi106@gmail.com>
2025-05-20 15:23:25 +0200

    Added a delete task operation but still need to use the task ID and the date key to delete task on localDB

--- src/model/Operations.ts
+++ src/model/Operations.ts
@@ -19,6 +19,36 @@ export async function viewTasks(): Promise<void>{
     console.log(LocalDB.instance.toString());
 }
 
+
+export async function deleteTasks(Ask: (q: string) => Promise<string>): Promise<void>{
+    let count = 0;
+    const keys = LocalDB.instance.GetDBKeys();
+    for (const key in keys) {
+        console.log(`${count}: ${key}`);
+        count++;
+    }
+    const taskDate = await Ask("Enter the date when you created the task? >");
+
+    try{
+        const listOfTasks = LocalDB.instance.GetTasks(taskDate);
+        console.log(`These are the list of tasks on ${taskDate}\n"Select the ID of the task you want to delete`)
+        for(let i = 0; i < listOfTasks.length; i++){
+            console.log(`${i+1}: ${listOfTasks[i]}`)
+        }        
+        
+        const taskId = await Ask("Enter The selected ID: ")
+
+
+    }
+    catch {
+        console.log("You have entered an incorrect id, or the task doesn't exist");
+    }
+    
+}
+
+
+
+
 export async function saveToJson(): Promise<void>{
 
     const data = LocalDB.instance;
