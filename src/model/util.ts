929e5aea3bc64d2b5f86d3db91b9527d45f12527
Tyrese-Mv <musawenkosi106@gmail.com>
2025-05-25 15:17:24 +0200

    added exports for the type declarations

--- src/model/util.ts
+++ src/model/util.ts
@@ -1,6 +1,6 @@
 import { Task } from "./TaskClass";
-type TaskKey = string;
-type TaskStorage = Task[];
+export type TaskKey = string;
+export type TaskStorage = Task[];
 
 export interface DB {
     [key: TaskKey]: TaskStorage;
