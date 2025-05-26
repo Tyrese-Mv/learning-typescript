2479efd52f4fe9ecbf18e1edd8c7ac768e2e05ce
Tyrese-Mv <musawenkosi106@gmail.com>
2025-05-26 14:35:04 +0200

    fixed indentation

--- src/LocalDB.test.ts
+++ src/LocalDB.test.ts
@@ -3,55 +3,55 @@ import { Task } from './model/TaskClass';
 import { DB ,TaskKey, TaskStorage} from './model/util';
 
 describe('LocalDB', () => {
-  let db: LocalDB;
-  let task1: Task;
-  let task2: Task;
+    let db: LocalDB;
+    let task1: Task;
+    let task2: Task;
 
 
-  beforeEach(() => {
-    db = LocalDB.instance;
+    beforeEach(() => {
+        db = LocalDB.instance;
 
     
-    (db as any).database = {};
+        (db as any).database = {};
 
-    task1 = new Task('Test task 1');
-    task2 = new Task('Test task 2');
-  });
+        task1 = new Task('Test task 1');
+        task2 = new Task('Test task 2');
+    });
 
-  test('should add tasks and retrieve them by date key', () => {
-    db.AddTask(task1);
-    const key = task1.GetDateAsKey();
-    expect(db.GetDBKeys()).toContain(key);
-    expect(db.GetTasks(key)).toEqual([task1]);
-  });
+    test('should add tasks and retrieve them by date key', () => {
+        db.AddTask(task1);
+        const key = task1.GetDateAsKey();
+        expect(db.GetDBKeys()).toContain(key);
+        expect(db.GetTasks(key)).toEqual([task1]);
+    });
 
-  test('should remove a task by id', () => {
-    db.AddTask(task1);
-    db.AddTask(task2);
-    const key = task1.GetDateAsKey();
-
-
-    db.RemoveTaskById(key, task1.GetTaskID());
-    const tasks = db.GetTasks(key);
-    expect(tasks).not.toContain(task1);
-    expect(tasks).toContain(task2);
-  });
-
-  test('should update task completion', () => {
-    db.AddTask(task1);
-    const key = task1.GetDateAsKey();
-
-    expect(task1.ToString()).toContain('Incomplete');
+    test('should remove a task by id', () => {
+        db.AddTask(task1);
+        db.AddTask(task2);
+        const key = task1.GetDateAsKey();
 
 
-    db.CompleteTask(key, task1.GetTaskID());
-    expect(task1.ToString()).toContain('Done');
-  });
+        db.RemoveTaskById(key, task1.GetTaskID());
+        const tasks = db.GetTasks(key);
+        expect(tasks).not.toContain(task1);
+        expect(tasks).toContain(task2);
+    });
 
-  test('toString should return a valid JSON representation of the database', () => {
-    db.AddTask(task1);
-    const jsonOutput = db.toString();
-    expect(jsonOutput).toContain(task1.GetTaskID());
-    expect(jsonOutput).toContain(task1.GetTaskDescription());
-  });
+    test('should update task completion', () => {
+        db.AddTask(task1);
+        const key = task1.GetDateAsKey();
+
+        expect(task1.ToString()).toContain('Incomplete');
+
+
+        db.CompleteTask(key, task1.GetTaskID());
+        expect(task1.ToString()).toContain('Done');
+    });
+
+    test('toString should return a valid JSON representation of the database', () => {
+        db.AddTask(task1);
+        const jsonOutput = db.toString();
+        expect(jsonOutput).toContain(task1.GetTaskID());
+        expect(jsonOutput).toContain(task1.GetTaskDescription());
+    });
 });
